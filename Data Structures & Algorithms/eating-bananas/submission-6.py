cc04334f80fd78c14c2c5ad244f496935c82404f
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-05-16 00:59:20 -0500

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def isCorrect(bananas_per_hour):
+            return sum(math.ceil(pile / bananas_per_hour) for pile in piles) <= h
+
+        low, high = 1, max(piles)
+        while low < high:
+            mid = (low + high) // 2
+            if isCorrect(mid):
+                high = mid
+            else:
+                low = mid + 1
+        return low
