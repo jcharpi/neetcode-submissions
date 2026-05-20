4a4e37d32a0fc73dc5922e2c5fd164c8f3c57d2e
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-05-20 18:27:21 -0500

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l, r = 0, len(s1) - 1
+        s1_counts = Counter(s1)
+        while r < len(s2):
+            if s1_counts == Counter(s2[l:r + 1]):
+                return True
+            l += 1
+            r += 1
+        return False
