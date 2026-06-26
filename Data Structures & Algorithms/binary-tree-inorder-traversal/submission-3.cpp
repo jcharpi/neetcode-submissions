2d9c105c49d63aad09dce43ef00cbd466e85589e
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-06-26 16:06:36 -0500

    Add: binary-tree-inorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-3.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> out;
+        stack<TreeNode*> s;
+        TreeNode* curr = root;
+
+        while (curr || !s.empty()) {
+            if (curr) {
+                s.push(curr);
+                curr = curr->left;
+            } else {
+                curr = s.top();
+                s.pop();
+                out.push_back(curr->val);
+                curr = curr->right;
+            }
+        }
+        return out;
+    }
+};
