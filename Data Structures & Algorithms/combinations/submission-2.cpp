74b3f300d0c0c08c0bbc6c17d55fdab3bbee55a8
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-07-28 21:06:40 -0500

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    void dfs(int i, vector<int>& curr, vector<vector<int>>& out, int n, int k) {
+        if (curr.size() == k) {
+            out.push_back(curr);
+            return;
+        }
+
+        if (i > n) return;
+
+        curr.push_back(i);
+        dfs(i + 1, curr, out, n, k); // include
+
+        curr.pop_back();
+        dfs(i + 1, curr, out, n, k);
+    }
+    
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> out;
+        vector<int> curr;
+        dfs(1, curr, out, n, k);
+        return out;
+    }
+};
