cdb571185fa6077c3a0784c5c9a93c0e121afd6c
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-08-12 18:28:07 -0500

    Add: palindrome-partitioning - submission-4

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> out;
+        vector<string> curr;
+
+        auto is_palindrome = [&](int l, int r) -> bool {
+            while (l < r) {
+                if (s[l] != s[r]) return false;
+                l++;
+                r--;
+            }
+            return true;
+        };
+
+        function<void(int i)> dfs = [&](int i) -> void {
+            if (i == ssize(s)) {
+                out.push_back(curr);
+                return;
+            }
+
+            for (int j = i; j < static_cast<int>(s.size()); j++) {
+                if (is_palindrome(i, j)) {
+                    curr.push_back(s.substr(i, j - i + 1));
+                    dfs(j + 1);
+                    curr.pop_back();
+                }
+            }
+        };
+
+        dfs(0);
+        return out;
+    }
+};
+    
