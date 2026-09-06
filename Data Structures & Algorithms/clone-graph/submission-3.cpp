359fa56ea90c057e55f8040a5fd01311b8cd17e1
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-09-06 14:27:23 -0500

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+		unordered_map<Node*, Node*> original_to_clone;
+		
+		function<Node*(Node* original)> dfs = [&](Node* original) -> Node* {
+			if (original_to_clone.contains(original)) return original_to_clone[original];
+
+			Node* clone = new Node(original->val);
+			original_to_clone[original] = clone;
+			for (Node* neighbor : original->neighbors) clone->neighbors.push_back(dfs(neighbor));
+			return clone;
+		};
+
+		return dfs(node);
+    }
+};
