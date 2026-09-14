d532e798d50664c456913a816cb072a7f180b062
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-09-14 18:45:11 -0500

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.py
@@ -0,0 +1,18 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key = lambda interval : interval.start)
+
+        latest_end = 0
+        for interval in intervals:
+            if interval.start < latest_end:
+                return False
+            latest_end = interval.end
+        return True
