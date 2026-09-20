9f1299d0cb01d5dd6dbe41cdf9324df888cd8100
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-09-20 12:42:30 -0500

    Add: merge-triplets-to-form-target - submission-3

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-3.py
@@ -0,0 +1,34 @@
+# Given
+# - 2D array of integers triplets
+# - triplets[i] = [ai, bi, ci]
+
+# - target = [x, y, z] which is the triplet we want to obtain
+
+# Operations
+# - Choose two different triplets triplets[i] and triplets[j]
+# -- Update triplets[j]: [max(ai, aj), max(bi, bj), max(ci, cj)].
+
+# DP intuition: dp[i] represents *WHAT*
+# - What do we know?
+# -- If, while iterating, we can make the triplet at some point, we can just return True
+# -- If some value ai, bi, ci is greater than the target respective value, then that whole triplet cannot be used with the update operation
+# --- I would be inclined to think dp[i] represents the most updated triplet we can have that does not violate this point, then by the end we return dp[-1] == target
+
+
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        dp = [[0, 0, 0] for _ in range(len(triplets) + 1)]
+
+        for i in range(1, len(triplets) + 1):
+            a, b, c = triplets[i - 1]
+            if (a <= target[0] and 
+                b <= target[1] and 
+                c <= target[2]):
+                dp[i] = [max(dp[i - 1][0], a), 
+                    max(dp[i - 1][1], b),
+                    max(dp[i - 1][2], c)]
+                if dp[i] == target:
+                    return True
+            else:
+                dp[i] = dp[i - 1]
+        return False
