d84266b5abb971e9ccbd76d94552dac0fac53ac4
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-09-24 21:54:49 -0500

    Add: lemonade-change - submission-3

---
+++ Data Structures & Algorithms/lemonade-change/submission-3.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool LemonadeChange(int[] bills) {
+        int fives = 0, tens = 0;
+
+        foreach (int bill in bills) {
+            if (bill == 5) fives++;
+            else if (bill == 10) {
+                if (fives == 0) return false;
+                fives--;
+                tens++;
+            }
+            else {
+                if (tens > 0 && fives > 0) {
+                    tens--;
+                    fives--;
+                }
+                else if (fives >= 3) fives -= 3;
+                else return false;
+            }
+        }
+
+        return true;
+    }
+}
